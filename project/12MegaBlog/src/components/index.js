9cd99e8aeea8ea8784694e1273454f4f1fa44f54
Purohit Naresh <65901402+Mspurohit15@users.noreply.github.com>
2024-11-30 19:45:47 +0530

    Third commit

--- project/12MegaBlog/src/components/index.js
+++ project/12MegaBlog/src/components/index.js
@@ -1,9 +1,18 @@
+import Select from "./Select";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
 import Container from "./container/container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn"
 import RTE from "./RTE.JSX";
+import Signup from "./Signup";
+import Login from "./Login";
+import PostFrom from "./post-form/PostFrom"
+import Button from "./Button";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Input from "./Input";
+
 
 export {
     Header,
@@ -11,5 +20,14 @@ export {
     Container,
     Logo,
     LogoutBtn,
-    RTE
+    RTE,
+    Login,
+    Signup,
+    PostFrom,
+    Select,
+    Button,
+    PostCard,
+    AuthLayout, 
+    Input
 }
+
