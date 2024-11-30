53ccc26d4db5f57ce28f5e5edc38d29603e1453b
Purohit Naresh <65901402+Mspurohit15@users.noreply.github.com>
2024-11-30 18:45:27 +0530

    Second commit

--- project/12MegaBlog/src/components/index.js
+++ project/12MegaBlog/src/components/index.js
@@ -1,7 +1,15 @@
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
+import Container from "./container/container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn"
+import RTE from "./RTE.JSX";
 
 export {
     Header,
-    Footer
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE
 }
